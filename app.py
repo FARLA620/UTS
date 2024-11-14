5e66c8cee4af7079d89847453a92710fc9ae9b90
FARLA620 <farlaedydiadin@students.amikom.ac.id>
2024-11-14 19:46:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from config import Config
 from models import db
 from flask_migrate import Migrate
 from controllers.student_controller import student_bp
-from controllers.enrollment_controller import enrollment_bp  # Impor blueprint enrollment
-from controllers.course_controller import course_bp  # Impor blueprint course
+from controllers.enrollment_controller import enrollment_bp
+from controllers.course_controller import course_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,15 +17,12 @@ with app.app_context():
 
 
 app.register_blueprint(student_bp)
-app.register_blueprint(enrollment_bp)  # Daftarkan blueprint enrollment
-app.register_blueprint(course_bp)  # Daftarkan blueprint course
-
+app.register_blueprint(enrollment_bp)
+app.register_blueprint(course_bp)
 @app.errorhandler(Exception)
 def handle_exception(e):
-    # Mencetak pesan kesalahan ke log
     app.logger.error(f"Error occurred: {str(e)}")
     
-    # Mengembalikan respons JSON
     return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
 
 
